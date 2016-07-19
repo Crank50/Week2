b8073c2c5d54ea89c322cf7486c899eb6a2fd04d
Justin <Crank50@icloud.com>
2016-07-18 22:41:51 -0600

    Initial commit

---
+++ day1/Day1TimesTable.java
@@ -0,0 +1,37 @@
+package Week2.day1;
+
+/**
+ * Created by Justin on 7/18/16.
+ */
+
+    public class Day1TimesTable {
+
+    public static void main(String[] args) {
+        int tableSize = 200;
+        printTimesTable(tableSize);
+    }
+
+    public static void printTimesTable(int tableSize) {
+        // first print the top header row using a for-loop
+        System.out.format("      ");
+        for (int i = 1; i <= tableSize; i++) {
+            System.out.format("%4d", i);
+        }
+        System.out.println();
+        System.out.println("--------------------------------------------------------");
+
+        // create another for-loop to start building the rows
+        for (int j = 1; j <= tableSize; j++) {
+            // print left most column first
+            System.out.format("%4d |", j);
+
+            // create another loop to fill in the multiplied columns
+            for (int y = 1; y <= tableSize; y++) {
+                System.out.format("%4d", j * y);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ day1/day1Fizzbuzz.java
@@ -0,0 +1,61 @@
+package Week2.day1;
+
+/**
+ * Created by Justin on 7/18/16.
+ */
+public class day1Fizzbuzz {
+
+    public static void main(String[] args) {
+        thisWay();
+        thatway();
+    }
+
+
+    public static void thisWay() {
+        // count from 1 to 100 (loop?)
+        for (int i = 1; i <= 100; i++) {
+            String returnThis = "";
+            // check if divisible by 3 -- print Fizz
+            if (i % 3 == 0) {
+                returnThis += "fizz";
+            }
+            // check if divisible by 5 -- print Buzz
+            else if (i % 5 == 0) {
+                returnThis += "buzz";
+            } //else if ((i % 3 == 0) && (i % 5 == 0)) {
+            //   returnThis = "fizzbuzz";
+            //}
+
+            if (returnThis.isEmpty()) {
+                System.out.println(i);
+            } else {
+                System.out.println(returnThis);
+            }
+        }
+
+    }
+
+    public static void thatway() {
+        // count from 1 to 100 (loop?)
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+
+
+
+        // if we have a word, print it, else print the number
+
+
+
+
